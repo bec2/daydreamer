dd2b3021937f87123cff5ddbee4ce37ac7a67e38
Beckett Hamilton <hamiltr2@aston.ac.uk>
2020-10-23 17:19:18 +0100

    adding some rudimentary button functionality

--- gui.py
+++ gui.py
@@ -12,25 +12,40 @@ w = bg_image.width()
 h = bg_image.height()
 
 #size window correctly for image
-root.geometry("%dx%d+50+30" % (w, h))
+root.geometry("500x400")
 cv = tk.Canvas(width=w, height=h)
 cv.pack(side='top', fill='both', expand='yes')
 cv.create_image(0,0,image=bg_image,anchor='nw')
 
 #add canvas text
-cv.create_text(15,20,text="Hello World",fill="red",anchor='nw')
-#random line
+cv.create_text(15,20,text="Hey there daydreamer.",fill="white",anchor='nw')
+cv.create_text(15,40,text="Need help with something?", fill="white",anchor='nw')
+
+# idea generator
 def idea():
     textidea=None
     for line in fileinput.input('textlist.txt'):
         if random.randrange(fileinput.lineno())==0:
             textidea = line
-    cv.create_text(15,60,text=textidea,fill="white",anchor='nw')
+    label = tk.Label(root, text=textidea)
+
+#clear the text
+def reset():
+    for label in idea():
+        label.destroy()
+
+# get help
+def needhelp():
+    print('Coming soon')
+
+        
     
 #add buttons
-btn1 = tk.Button(cv, text="Click", command=idea)
+btn1 = tk.Button(cv, text="Inspire Me", command=idea)
 btn1.pack(side='left', padx=10, pady=5, anchor='sw')
-btn2 = tk.Button(cv, text="Quit", command=root.destroy)
+btn2 = tk.Button(cv, text="I Need Help", command=needhelp)
 btn2.pack(side='left',padx=10,pady=5,anchor='sw')
+btn4 = tk.Button(cv, text="Quit", command=root.destroy)
+btn4.pack(side='left',padx=10,pady=5,anchor='sw')
 root.mainloop()
 
